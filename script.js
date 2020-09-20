0d0b233a740b551dac8eebbb6188f100a2333517
Laurie <lauriejennifer@gmail.com>
2020-09-19 23:14:13 -0500

    Cleaned up some extra comments in the JS file

--- script.js
+++ script.js
@@ -85,20 +85,6 @@ window.addEventListener("load", function() {
       }
       event.preventDefault();
       }
-      //THIS DOES NOT WORK. IN FACT, NOW THE WHOLE PAGE DOESN'T WORK
-      /*fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
-         response.json().then(function(json) {
-         document.getElementById("missionTarget").innerHTML = <h2>Mission Destination</h2>
-      <ol>
-         <li>Name: ${json[5].name}</li>
-         <li>Diameter: ${json[5].diameter}</li>
-         <li>Star: ${json[5].star}</li>
-         <li>Distance from Earth: ${json[5].distance}</li>
-         <li>Number of Moons: ${json[5].moons}</li>
-      </ol>
-      <img src= "${json[5].image}"></img>;
-         });
-      });*/      
    });
 
 /*This block of code shows how to format the HTML once you fetch some planetary JSON!
